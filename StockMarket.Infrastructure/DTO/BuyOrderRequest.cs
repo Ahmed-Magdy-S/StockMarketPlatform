3783a216f53c70c82a820060b4770af02d02c0fa
Ahmed Magdy Seleem <Ahmed.Magdy.S.19@gmail.com>
2023-05-01 16:38:38 +0300

    refactor files structure

---
+++ StockMarket.Infrastructure/DTO/BuyOrderRequest.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using StockMarket.Core.Entities;
+using StockMarket.Infrastructure.Utils;
+
+namespace StockMarket.Infrastructure.DTO
+{
+    //TODO: You may use fluent validation instead of annotations
+    public class BuyOrderRequest
+    {
+        [Required]
+        public string? StockSymbol { get; set; }
+
+        [Required]
+        public string? StockName { get; set;}
+      
+        [MinDate("01/01/2000")]
+        public DateTime DateAndTimeOfOrder { get; set; }
+
+        [Range(1, 100000)]
+        public uint Quantity { get; set; }
+
+        [Range(1.0, 10000.0)]
+        public double Price { get; set; }
+
+        public BuyOrder ToBuyOrder()
+        {
+            return new BuyOrder
+            {
+                StockName = StockName,
+                Id = Guid.NewGuid(),
+                StockSymbol = StockSymbol,
+                Quantity = Quantity,
+                Price = Price,
+                DateAndTimeOfOrder = DateAndTimeOfOrder
+
+            };
+        }
+    }
+}
